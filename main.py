13c80785d4e2d2a8d125cfc916b43e8b30c7f3f2
alferio <ioan-cristian.alferaru@ec.europa.eu>
2024-07-02 18:50:09 +0200

    feat(snaps):

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 import math
 import time
+from typing import List
+
 import ezdxf
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QWidget, QPushButton,
     QColorDialog, QSpinBox, QListWidget, QListWidgetItem,
-    QLineEdit, QCheckBox, QHBoxLayout, QVBoxLayout, QDialog, QRadioButton, QStyle,
+    QLineEdit, QCheckBox, QHBoxLayout, QVBoxLayout, QDialog, QRadioButton, QStyle, QLabel,
 )
 from PySide6.QtGui import QPainter, QPen, QColor, QTransform
 from PySide6.QtCore import Qt, QPoint
@@ -16,6 +18,27 @@ lwindex = [0, 5, 9, 13, 15, 18, 20, 25,
            30, 35, 40, 50, 53, 60, 70, 80,
            90, 100, 106, 120, 140, 158, 200,
            ]
+
+
+def round_to_nearest(value, base):
+    return base * round(value / base)
+
+
+def distance(point1, point2):
+    return math.sqrt((point1.x() - point2.x()) ** 2 + (point1.y() - point2.y()) ** 2)
+
+
+def find_nearest_point(point_list: List[QPoint], p: QPoint) -> QPoint:
+    nearest_point = point_list[0]
+    min_distance = distance(point_list[0], p)
+
+    for point in point_list[1:]:
+        dist = distance(point, p)
+        if dist < min_distance:
+            nearest_point = point
+            min_distance = dist
+
+    return nearest_point
 
 
 class Line:
@@ -84,13 +107,15 @@ class Layer:
         self.width = width
         self.visible = visible
         self.lines = []
+        self.flAutoCut = True
 
     def add_line(self, line):
         self.lines.append(line)
         self.cleanup()
 
     def cleanup(self):
-        self.rescan_intersections()
+        if self.flAutoCut:
+            self.rescan_intersections()
         self.remove_short_lines()
         self.cleanup_duplicates()
 
@@ -142,7 +167,7 @@ class Layer:
         self.lines = [line for line in self.lines if not line.is_short()]
 
 
-class Canvas(QWidget):
+class DrawingManager(QWidget):
     def __init__(self):
         super().__init__()
         self.layers = [Layer(name="Layer-0")]
@@ -151,6 +176,11 @@ class Canvas(QWidget):
         self.zoom_factor = 1.0
         self.offset = QPoint(0, 0)
         self.dxf_file = None
+        self.flSnapGrid = True
+        self.gridSpacing = QPoint(10, 10)
+        self.flSnapPoints = True
+        self.snapDistance = 5
+        self.snapped_pointer = QPoint(0, 0)
 
     def set_current_layer(self, index):
         self.current_layer_index = index
@@ -210,7 +240,9 @@ class Canvas(QWidget):
                     (line.start_point.x(), line.start_point.y()),
                     (line.end_point.x(), line.end_point.y()),
                     dxfattribs={
-                        'layer': layer.name
+                        'layer': layer.name,
+                        'color': self.qcolor_to_dxf_color(layer.color),
+                        'lineweight': lwindex[layer.width]
                     }
                 )
 
@@ -236,24 +268,39 @@ class Canvas(QWidget):
     def map_to_view(self, point):
         return point * self.zoom_factor + self.offset
 
+    def current_layer(self, ):
+        return self.layers[self.current_layer_index]
+
+    def apply_mouse_input_modifiers(self, pos: QPoint) -> QPoint:
+        p = QPoint(pos.x(), pos.y())
+        if self.flSnapPoints:
+            nearest_point = find_nearest_point(self.get_all_points(), pos)
+            if nearest_point is not None and distance(nearest_point, pos) <= self.snapDistance:
+                p = QPoint(nearest_point.x(), nearest_point.y())
+        if self.flSnapGrid:
+            p = QPoint(round_to_nearest(pos.x(), self.gridSpacing.x()), round_to_nearest(pos.y(), self.gridSpacing.y()))
+        self.snapped_pointer = QPoint(p.x(), p.y())
+        return p
+
     def mousePressEvent(self, event):
         scene_pos = self.map_to_scene(event.pos())
         if event.button() == Qt.LeftButton:
+            scene_pos = self.apply_mouse_input_modifiers(scene_pos)
             layer = self.layers[self.current_layer_index]
             self.current_line = Line(scene_pos, scene_pos, layer.color, layer.width)
         elif event.button() == Qt.RightButton:
-            for layer in self.layers:
-                if not layer.visible:
-                    continue
-                for line in layer.lines:
-                    if line.contains_point(scene_pos):
-                        layer.lines.remove(line)
-                        self.update()
-                        self.save_dxf()
-                        return
+            clayer = self.current_layer()
+            for line in clayer.lines:
+                if line.contains_point(scene_pos):
+                    clayer.lines.remove(line)
+                    self.update()
+                    self.save_dxf()
+                    return
 
     def mouseMoveEvent(self, event):
         scene_pos = self.map_to_scene(event.pos())
+        scene_pos = self.apply_mouse_input_modifiers(scene_pos)
+        self.snapped_pointer=scene_pos
         if self.current_line:
             end_point = scene_pos
             if event.modifiers() & Qt.ControlModifier:
@@ -263,10 +310,11 @@ class Canvas(QWidget):
             layer = self.layers[self.current_layer_index]
             self.current_line.color = layer.color
             self.current_line.width = layer.width
-            self.update()
+        self.update()
 
     def mouseReleaseEvent(self, event):
         scene_pos = self.map_to_scene(event.pos())
+        scene_pos = self.apply_mouse_input_modifiers(scene_pos)
         if self.current_line:
             end_point = scene_pos
             if event.modifiers() & Qt.ControlModifier:
@@ -274,8 +322,8 @@ class Canvas(QWidget):
             self.current_line.end_point = end_point
             self.layers[self.current_layer_index].add_line(self.current_line)
             self.current_line = None
-            self.update()
             self.save_dxf()
+        self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -288,24 +336,35 @@ class Canvas(QWidget):
             if not layer.visible:
                 continue
             for line in layer.lines:
-                pen = QPen(line.color, line.width / self.zoom_factor, Qt.SolidLine)
+                pen = QPen(layer.color, layer.width / self.zoom_factor, Qt.SolidLine)
                 painter.setPen(pen)
                 painter.drawLine(line.start_point, line.end_point)
                 self.draw_cross(painter, line.start_point)
                 self.draw_cross(painter, line.end_point)
         if self.current_line:
-            pen = QPen(self.current_line.color, self.current_line.width / self.zoom_factor, Qt.SolidLine)
+            pen = QPen(self.current_layer().color, self.current_layer().width / self.zoom_factor, Qt.SolidLine)
             painter.setPen(pen)
             painter.drawLine(self.current_line.start_point, self.current_line.end_point)
             self.draw_cross(painter, self.current_line.start_point)
             self.draw_cross(painter, self.current_line.end_point)
+        self.draw_cursor(painter, self.snapped_pointer)
 
-    def draw_cross(self, painter, point):
+    def draw_cross(self, painter: QPainter, point: QPoint):
         pen = QPen(QColor(Qt.red), 2, Qt.SolidLine)
         painter.setPen(pen)
         size = 2
         painter.drawLine(point.x() - size, point.y() - size, point.x() + size, point.y() + size)
         painter.drawLine(point.x() + size, point.y() - size, point.x() - size, point.y() + size)
+
+    def draw_cursor(self, painter: QPainter, point: QPoint):
+        x=point.x()
+        y=point.y()
+        pen = QPen(QColor(Qt.black), 1, Qt.SolidLine)
+        painter.setPen(pen)
+        size = 5
+        painter.drawLine(x - 2 * size, y, x + 2 * size, y)
+        painter.drawLine(x, y - 2 * size, x, y + 2 * size)
+        painter.drawRect(x - size, y - size, 2*size, 2*size)
 
     def snap_to_angle(self, start_point, end_point):
         dx = end_point.x() - start_point.x()
@@ -318,6 +377,14 @@ class Canvas(QWidget):
             start_point.y() + length * math.sin(snap_angle)
         )
         return snapped_end_point
+
+    def get_all_points(self):
+        points = []
+        for layer in self.layers:
+            for line in layer.lines:
+                points.append(line.start_point)
+                points.append(line.end_point)
+        return points
 
 
 class LayerItem(QWidget):
@@ -347,10 +414,23 @@ class LayerItem(QWidget):
         self.color_button.clicked.connect(self.select_color)
         layout.addWidget(self.color_button)
 
+        self.visibility_label = QLabel()
+        self.visibility_label.setText("visible")
+        layout.addWidget(self.visibility_label)
+
         self.visibility_checkbox = QCheckBox()
         self.visibility_checkbox.setChecked(self.layer.visible)
         self.visibility_checkbox.stateChanged.connect(self.on_visibility_changed)
         layout.addWidget(self.visibility_checkbox)
+
+        self.visibility_label = QLabel()
+        self.visibility_label.setText("auto-cut")
+        layout.addWidget(self.visibility_label)
+
+        self.autocut_checkbox = QCheckBox()
+        self.autocut_checkbox.setChecked(self.layer.visible)
+        self.autocut_checkbox.stateChanged.connect(self.on_autocut_changed)
+        layout.addWidget(self.autocut_checkbox)
 
         self.remove_button = QPushButton("Remove")
         self.remove_button.clicked.connect(self.on_remove_clicked)
@@ -379,6 +459,9 @@ class LayerItem(QWidget):
     def on_visibility_changed(self, state):
         self.layer.visible = bool(state)
 
+    def on_autocut_changed(self, state):
+        self.layer.flAutoCut = bool(state)
+
     def on_remove_clicked(self):
         self.parent.remove_layer(self.layer)
 
@@ -406,8 +489,6 @@ class LayerManager(QDialog):
         for i, layer in enumerate(self.canvas.layers):
             item = QListWidgetItem()
             widget = LayerItem(layer, self)
-            if i == self.canvas.current_layer_index:
-                widget.setStyleSheet("background-color: lightblue;")
             item.setSizeHint(widget.sizeHint())
             self.layer_list.addItem(item)
             self.layer_list.setItemWidget(item, widget)
@@ -429,27 +510,53 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("PySide6 Drawing Application")
         self.setGeometry(100, 100, 800, 600)  # Initial window size
-        self.canvas = Canvas()
-        self.canvas.setStyleSheet("background-color: #000000;")
+        self.drawing_manager = DrawingManager()
+        self.drawing_manager.setStyleSheet("background-color: #000000;")
         if file_path:
-            self.canvas.load_dxf(file_path)
-            self.canvas.dxf_file = file_path
-        self.layer_manager = LayerManager(self.canvas)
+            self.drawing_manager.load_dxf(file_path)
+            self.drawing_manager.dxf_file = file_path
+        self.layer_manager = LayerManager(self.drawing_manager)
         self.layer_manager.show()  # Show the layer manager as a non-blocking modal
         self.init_ui()
+
+    def on_grid_snap_changed(self):
+        self.drawing_manager.flSnapGrid = not self.drawing_manager.flSnapGrid
+        self.statusBar().showMessage(f"flSnapGrid is {self.drawing_manager.flSnapGrid}")
+
+    def on_vertex_snap_changed(self):
+        self.drawing_manager.flSnapPoints = not self.drawing_manager.flSnapPoints
+        self.statusBar().showMessage(f"flSnapPoints is {self.drawing_manager.flSnapPoints}")
 
     def init_ui(self):
         main_layout = QVBoxLayout()
 
-        main_layout.addWidget(self.canvas)
+        # Add buttons and checkboxes
+        control_layout = QHBoxLayout()
+        grid_snap_checkbox = QCheckBox("Grid Snap")
+        grid_snap_checkbox.setChecked(True)
+        grid_snap_checkbox.stateChanged.connect(self.on_grid_snap_changed)
+        vertex_snap_checkbox = QCheckBox("Vertex Snap")
+        vertex_snap_checkbox.setChecked(True)
+        vertex_snap_checkbox.stateChanged.connect(self.on_vertex_snap_changed)
+        control_layout.addWidget(grid_snap_checkbox)
+        control_layout.addWidget(vertex_snap_checkbox)
+
+        main_layout.addLayout(control_layout)
+        main_layout.addWidget(self.drawing_manager)
 
         container = QWidget()
         container.setLayout(main_layout)
         self.setCentralWidget(container)
+        # Add status bar
+        self.statusBar().showMessage("Status: Ready")
 
         self.layer_manager.update_layer_list()
         self.layer_manager.layer_list.setCurrentRow(0)
-        self.canvas.set_current_layer(0)
+        self.drawing_manager.set_current_layer(0)
+
+    def closeEvent(self, event):
+        self.layer_manager.close()
+        event.accept()
 
 
 if __name__ == '__main__':
